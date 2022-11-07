6062f0e467848ebce836369e73cc18397e288c15
swchoi0 <=>
2022-11-07 22:59:58 +0900

    init

---
+++ src/main/java/com/getthejob/core/GetthejobApplication.java
@@ -0,0 +1,13 @@
+package com.getthejob.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetthejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetthejobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getthejob/core/GetthejobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getthejob.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetthejobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
